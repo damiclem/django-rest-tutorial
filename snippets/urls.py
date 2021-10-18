0ff330450e7967f91f45a06a269ba0b22c414392
Damiano <damiano.clementel@gmail.com>
2021-10-18 10:14:39 +0200

    Updated request response and automatic formatting

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 # Import views library
 from snippets import views
+# Define suffix patterns
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 # Define URL patterns
@@ -9,3 +11,7 @@ urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+
+# Append a set of available format suffix patterns
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -2,17 +2,26 @@
 from django.http import HttpResponse, JsonResponse
 # Define module for CSFR protection
 from django.views.decorators.csrf import csrf_exempt
+# Define module for Django decorators
+from rest_framework.decorators import api_view
 # Define JSON parser
 from rest_framework.parsers import JSONParser
+# Define response module
+from rest_framework.response import Response
+# Define response status handler
+from rest_framework import status
 # Define module for JSON parser
 from snippets.models import Snippet
 # Define serializer for JSON parser
 from snippets.serializers import SnippetSerializer
 
 
-# NOTE Here CSFR protection is disabled, i.e. token is not requested
-@csrf_exempt
-def snippet_list(request):
+# # NOTE Here CSFR protection is disabled, i.e. token is not requested
+# @csrf_exempt
+# NOTE @api_view decorator over a function define methods allowed to access it
+@api_view(['GET', 'POST'])
+# NOTE optional format suffix can be set to URLs
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
@@ -22,7 +31,9 @@ def snippet_list(request):
         # Define snippet serializer 
         serializer = SnippetSerializer(snippets, many=True)
         # Return JSON-formatted snippet
-        return JsonResponse(serializer.data, safe=False)
+        # return JsonResponse(serializer.data, safe=False)
+        # NOTE request.data can handle request both in JSON or other formats!
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         # Define JSON-formatted request body
@@ -34,33 +45,41 @@ def snippet_list(request):
             # Save serialized data
             serializer.save()
             # Return updated data, as JSON document
-            return JsonResponse(serializer.data, status=201)
+            # return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         # Otherwise, return error
-        return JsonResponse(serializer.errors, status=400)
+        # return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+# @csrf_exempt
+@api_view
+def snippet_detail(request, pk, formta=None):
     """
     Retrieve, update or delete a code snippet.
     """
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        # return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        # return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        # data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=request.data)  # data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            # return JsonResponse(serializer.data)
+            return Response(serializer.data)
+        # return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        # return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
